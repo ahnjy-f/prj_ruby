cd760c30cef2ea70715b44b785fdfe98833a5783
鶴田美奈 <tsuruta-mina@fndm41noMacBook-ea.local>
2020-04-22 16:54:23 +0900

    feature/003 add account(models/migrate/seeds)２

--- db/migrate/20200422002026_create_accounts.rb
+++ db/migrate/20200422002026_create_accounts.rb
@@ -3,7 +3,7 @@ class CreateAccounts < ActiveRecord::Migration[6.0]
     create_table :accounts do |t|
       t.references :member, null: false # 社員テーブル.ID
       t.string :mail_address, null: false
-      t.string :password_digest, null: false # ハッシュ化して登録
+      t.string :password, null: false # ハッシュ化して登録
       t.string :admin_flag, null: false # 0:一般、1:システム管理者
       t.datetime :created_at, null: false
       t.datetime :updated_at, null: false 
