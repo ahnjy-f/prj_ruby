85031ef2824e0fe54b5c64c8d95214091e97a50e
DongYeon Lee <dlehddus1285@gmail.com>
2020-04-23 01:42:49 +0000

    fix/010

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  namespace :account do
+    root "top#index"
+    get "login" => "session#new", as: :login
+    post "session" => "session#create", as: :session
+    delete "session" => "session#destroy"
+  end
+
+end
+
