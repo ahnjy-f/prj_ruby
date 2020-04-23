0cefa90c27358e5c1f6176d5af123e1169f6c577
ソン・べジョン <son-beajung@kanrishanoMacBook-Air.local>
2020-04-23 17:18:51 +0900

    put menu links

---
+++ app/controllers/account/passwords_controller.rb
@@ -0,0 +1,4 @@
+class Account::PasswordsController < Account::Base
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+    #追加
+    get "members/show" #フロフィール画面
+    get "passwords/show" #パスワード変更
+    get "members/index" #メンーバ一覧
   end
 
 end
