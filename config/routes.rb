145079adc860990f4280070fc4879de4edb5bd91
ahnjy-f <jeeyoung-ahn@froide.co.jp>
2020-04-25 23:09:22 +0900

    メンバーの更新ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,19 @@ Rails.application.routes.draw do
   namespace :account do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    resource :sessions, only: [ :create, :destroy] 
-    resource except: [ :new, :create, :destroy]
-    get "members/show" #フロフィール画面
+    resource :session, only: [ :create, :destroy] 
+    
+    # get "members/show" #フロフィール画面
     get "passwords/edit" => "passwords#edit" 
-    get "members/index" #メンーバ一覧
+    # get "members/index" #メンーバ一覧
     patch "passwords/edit" => "passwords#update"
+    
+    resources :members, except: [ :destroy ]
+
+    get "members/:id/edit" => "members#edit"
+    post "members/:id/edit" => "members#edit"
+    patch "members/:id" => "members#update"
+
   end
 
 end
