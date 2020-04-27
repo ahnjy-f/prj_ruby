0726bb3953547eb0ed74fb55b3d44cd06814b0c5
ahnjy-f <jeeyoung-ahn@froide.co.jp>
2020-04-27 13:25:17 +0900

    dbに直接データを入れている

--- app/controllers/account/members_controller.rb
+++ app/controllers/account/members_controller.rb
@@ -13,14 +13,17 @@ class Account::MembersController < Account::Base
     def edit
         @member = current_member
         @account = Account.find_by(id: session[:account_id])
-        
     end
 
     def update
         @account = Account.find_by(id: session[:account_id])
         @member = current_member
         @member.assign_attributes(member_params)
+
         if @member.save
+
+            save_history
+
             flash.notice = "情報更新ができました。"
             redirect_to :account_member
         else
@@ -45,6 +48,25 @@ class Account::MembersController < Account::Base
             :week_things, :happy_done_things, :disgusted_things,
             :freedom_message
         )
+    end
+
+    def save_history
+        edithistory = MemberEditHistory.new
+        edithistory.member_id = @member.id
+        edithistory.face_photo_path = @member.face_photo_path
+        edithistory.one_word_comment = @member.one_word_comment
+        edithistory.personality = @member.personality
+        edithistory.hobby = @member.hobby
+        edithistory.favorite_things = @member.favorite_things
+        edithistory.hate_things = @member.hate_things
+        edithistory.strong_point = @member.strong_point
+        edithistory.week_point = @member.week_point
+        edithistory.special_skill = @member.special_skill
+        edithistory.week_things = @member.week_things
+        edithistory.happy_done_things = @member.happy_done_things
+        edithistory.disgusted_things = @member.disgusted_things
+        edithistory.freedom_message = @member.freedom_message
+        edithistory.save
     end
 
 end
