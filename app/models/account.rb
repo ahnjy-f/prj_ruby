c9ef2cd98d1506d4504c6d5d5f1918fa7015b0e5
ahnjy-f <63025949+ahnjy-f@users.noreply.github.com>
2020-04-28 14:44:01 +0900

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,9 +2,7 @@ class Account < ApplicationRecord
     belongs_to :member, class_name: "Member", foreign_key: "member_id", optional: true
 
     # validates :member_id, presence: true
-
-    validates :member_id, presence: true
-
+    
     def password=(raw_password)
         if raw_password.kind_of?(String)
             self.password_digest = BCrypt::Password.create(raw_password)
