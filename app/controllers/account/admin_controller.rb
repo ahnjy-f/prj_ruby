13eabcd15fb1e4880315a7993271587c7aa93268
Tsuruta Mina <60842701+minaTsuruta@users.noreply.github.com>
2020-04-28 12:21:34 +0900

    Update admin_controller.rb

--- app/controllers/account/admin_controller.rb
+++ app/controllers/account/admin_controller.rb
@@ -13,5 +13,27 @@ class Account::AdminController < Account::Base
         # HitProduct.select("hit_products.id hit_product_id, book_marks.id book_mark_id, hit_q").left_joins(:book_marks)
         @account = current_account
         @member = current_member
-    end 
+        
+        @m = @m.page(params[:page])
+        end
+    
+    def edit 	
+        @m = Member.find(params[:id])	
+        pp @m.account.mail_address	
+    end	
+
+    def update	
+        @m = Member.find(params[:id])	
+        @m.assign_attributes(params[:member])	
+    # pp "================="	
+    # pp @m.assign_attributes(params[:member])    	
+    # pp "================="	
+    #     if @m.save	
+    #         flash.notice = "編集成功"	
+    #         redirect_to:account_root	
+    #     else	
+    #         flash.notice = "編集成功"	
+    #         redirect_to:account_root	
+    #     end	
+    end	
 end
